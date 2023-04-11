d5566942e3ae703ddb6c635f093669eb94d4f45e
Victor Ogata <111644685+VictorOgataj8@users.noreply.github.com>
2023-04-11 11:39:56 -0300

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,51 @@
+export enum USER_ROLES {
+  NORMAL = "normal",
+  ADMIN = "admin",
+}
+
+export interface TokenPayload {
+  id: string;
+  name: string;
+  role: USER_ROLES;
+}
+
+export interface PostModel {
+  id: string;
+  content: string;
+  likes: number;
+  dislikes: number;
+  createdAt: string;
+  updatedAt: string;
+  creator: {
+    id: string;
+    name: string;
+  };
+}
+
+export interface postDB {
+    id: string,
+    creator_id: string,
+    content: string,
+    likes: number ,
+    dislikes: number ,
+    created_at: string,
+    updated_at: string 
+};
+
+export interface UserDB {
+    id:string,
+    name:string,
+    email:string,
+    password:string,
+    role: USER_ROLES,
+    created_at:string 
+}
+
+export interface UserModel{
+    id:string,
+    name:string,
+    email:string,
+    password:string,
+    role:USER_ROLES,
+    createdAt:string
+}
